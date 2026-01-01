73c54519e929ac2e3fd7d5b57d9da9b4ba85b85e
jcs <jaredscruggs@outlook.com>
2026-01-01 12:56:26 -0500

    feat: create spring project

---
+++ src/test/java/com/dewdrop/hintly/HintlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dewdrop.hintly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HintlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
